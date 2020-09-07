b4c32efc30bd13ffbf6ee661ad35d71f086c6eeb
Eduard Tolosa <edu4rdshl@protonmail.com>
2020-09-07 03:43:20 -0500

    Set --timeout 30000

--- src/browser/process.rs
+++ src/browser/process.rs
@@ -116,7 +116,7 @@ impl<'a> LaunchOptions<'a> {
 
 /// These are passed to the Chrome binary by default.
 /// Via https://github.com/GoogleChrome/puppeteer/blob/master/lib/Launcher.js#L38
-static DEFAULT_ARGS: [&str; 24] = [
+static DEFAULT_ARGS: [&str; 25] = [
     "--disable-background-networking",
     "--enable-features=NetworkService,NetworkServiceInProcess",
     "--disable-background-timer-throttling",
@@ -142,6 +142,7 @@ static DEFAULT_ARGS: [&str; 24] = [
     "--password-store=basic",
     "--use-mock-keychain",
     "--ignore-certificate-errors",
+    "--timeout 30000",
 ];
 
 impl Process {
